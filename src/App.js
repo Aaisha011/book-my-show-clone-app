1007830d5c487787d9e38fef8810107aa7094837
Aaisha Khan <160912968+Aaisha011@users.noreply.github.com>
2024-05-10 20:50:12 +0000

    files are added

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+// React Slick CSS
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+// import {Routes,Route} from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      {/* <Routes> 
+        <Route path="/" element={<Home Page/>}/>
+        <Route path="/movie:id" element={<Movie Page/>}/>
+        <Route path="/plays" element={<Plays Page/>}/>
+      </Routes> */}
+    </div>
+  );
+}
+
+export default App;
